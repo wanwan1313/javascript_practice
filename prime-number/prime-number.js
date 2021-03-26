0d0022af4ca4ea67c3726e93884283fc8e685ada
wanwan <wanwan1313@gmail.com>
2021-03-26 23:56:03 +0800

    修改成可以輸入範圍的程式碼

--- prime-number/prime-number.js
+++ prime-number/prime-number.js
@@ -1,29 +1,38 @@
-let prime_number = []
+let btn = document.querySelector('button')
+btn.addEventListener('click',function(){
+    let prime_number = []
+    let in1 = document.querySelector('#in1').value
+    let in2 = document.querySelector('#in2').value
+    console.log(in1, in2)
 
-for (let i = 2; i <= 100; i++) {
+    for (let i = Number(in1); i <= Number(in2); i++) {
 
-    let prime = true　　//先設定都是質數=true
+        let prime = true　　//先設定都是質數=true
 
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-            prime = false　　//一旦會被整除，就判定質數=false
-            break
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                prime = false　　//一旦會被整除，就判定質數=false
+                break
+            }
         }
-    }
 
-    if (prime == true) {
+        if (prime == true) {
 
-        prime_number.push(i)
+            prime_number.push(i)
 
-    }
+        }
 
-};
+    };
 
-console.log(prime_number)
+    console.log(prime_number)
 
 
-let p = document.querySelector('p')
-p.innerText = prime_number.join(',')
+    let p = document.querySelector('p')
+    p.innerText = prime_number.join(',')
+})
+
+
+
 
 let a = [3,4]
 let b = [5]
